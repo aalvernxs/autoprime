9513a653393e690c77cc1e9d4cc617a4ece05fbb
Arthur Alvernaz <arthuralvernaz1201@outlook.com>
2026-04-25 12:23:17 -0300

    feat: new search component

--- frontend-angular/src/app/search/search.ts
+++ frontend-angular/src/app/search/search.ts
@@ -14,7 +14,28 @@ export class Search {
   anoSelecionado = '';
   precoSelecionado = '';
 
+  dropdownAberto: string | null = null;
+
   marcas = ['Toyota', 'Honda', 'Ford', 'Chevrolet', 'BMW', 'Audi', 'Mercedes-Benz', 'Volkswagen', 'Nissan', 'Hyundai'];
-  anos = ['2021, 2022, 2023, 2024', '2026'];
+  anos = ['2021', '2022', '2023', '2024', '2026'];
   precos = ['Até R$ 50.000', 'R$ 50.000 - R$ 100.000', 'R$ 100.000 - R$ 200.000', 'Acima de R$ 200.000'];
+
+  toggleDropdown(categoria: string) {
+    this.dropdownAberto = this.dropdownAberto === categoria? null : categoria;
+  }
+
+  selecionar(campo: string, valor: string) {
+    if (campo === 'marca') this.marcaSelecionada = valor;
+    if (campo === 'ano') this.anoSelecionado = valor;
+    if (campo === 'preco') this.precoSelecionado = valor;
+    this.dropdownAberto = null;
+  }
+
+  limpar(campo: string){
+    if (campo === 'marca') this.marcaSelecionada = '';
+    if (campo === 'ano') this.anoSelecionado = '';
+    if (campo === 'preco') this.precoSelecionado = '';
+    this.dropdownAberto = null;
+  }
+  
 }
